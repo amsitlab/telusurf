f8468b20178e262fcccc0340f421d8a52e3bc4a1
Amsit S <10000281+amsitlab@users.noreply.github.com>
2026-05-06 05:57:22 +0700

    Center launcher logo with theme-aware tint

--- app/src/main/java/amsitlab/android/telusurf/MainActivity.java
+++ app/src/main/java/amsitlab/android/telusurf/MainActivity.java
@@ -1,8 +1,13 @@
 package amsitlab.android.telusurf;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ivLogo = findViewById(R.id.ivLogo);
+
+        boolean isDarkMode =
+                (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
+                        == Configuration.UI_MODE_NIGHT_YES;
+
+        ImageViewCompat.setImageTintList(ivLogo, isDarkMode ? null : getColorStateList(android.R.color.black));
+
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        int logoSize = (int) (Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels) * 0.42f);
+
+        ivLogo.getLayoutParams().width = logoSize;
+        ivLogo.getLayoutParams().height = logoSize;
+        ivLogo.requestLayout();
     }
 }
