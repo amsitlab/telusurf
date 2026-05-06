747cff4b3a17d33a9b95e5a48cca54c1076a241b
Amsit S <10000281+amsitlab@users.noreply.github.com>
2026-05-06 23:49:26 +0700

    Add basic toolbar and menu popup toggle

--- app/src/main/java/amsitlab/android/telusurf/MainActivity.java
+++ app/src/main/java/amsitlab/android/telusurf/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupToolbarMenu();
         onCreateLogo();
     }
 
@@ -32,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, SettingsActivity.class));
     }
 
+
+    private void setupToolbarMenu() {
+        View toolbar = findViewById(R.id.toolbar);
+        View toolbarMenuPopup = findViewById(R.id.toolbarMenuPopup);
+        ImageButton toolbarMenu = findViewById(R.id.toolbarMenu);
+        TextView menuSettings = findViewById(R.id.menuSettings);
+
+        toolbarMenu.setOnClickListener(v -> {
+            toolbar.setVisibility(View.GONE);
+            toolbarMenuPopup.setVisibility(View.VISIBLE);
+        });
+
+        menuSettings.setOnClickListener(v -> {
+            toolbarMenuPopup.setVisibility(View.GONE);
+            toolbar.setVisibility(View.VISIBLE);
+            openSettings();
+        });
+    }
     private void onCreateLogo() {
         ImageView ivLogo = findViewById(R.id.ivLogo);
 
