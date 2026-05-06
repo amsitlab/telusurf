6b8faa6809bead6d5b9f0f48894dad1c9ca005c6
Amsit S <10000281+amsitlab@users.noreply.github.com>
2026-05-06 22:15:56 +0700

    Refactor logo setup into onCreateLogo method

--- app/src/main/java/amsitlab/android/telusurf/MainActivity.java
+++ app/src/main/java/amsitlab/android/telusurf/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        onCreateLogo();
+    }
+
+    private void onCreateLogo() {
         ImageView ivLogo = findViewById(R.id.ivLogo);
 
         boolean isDarkMode =
